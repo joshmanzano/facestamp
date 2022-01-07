e426a87c1b049e337a7a564289791866334a9004
joshmanzano <josh_manzano@dlsu.edu.ph>
2022-01-07 17:36:00 +0800

    changing test.py drastically

--- run_steganogan.py
+++ run_steganogan.py
@@ -1,213 +1,65 @@
-import subprocess 
 from glob import glob
-from PIL import Image, ImageOps
-import numpy as np
-import os
 import pathlib
-import torch
-from torch import nn
-import ast
 from tqdm import tqdm
 import pickle
-import random
-import sys
-import json
 from steganogan.models import SteganoGAN
-import random
 from numpy import dot
 from numpy.linalg import norm
 import argparse
 
-
-base_path = './test_data/vidtimit/process'
-
-df_models = json.load(open('df_models.json','r')) 
-
-for df_model in df_models:
-    input_path = base_path + df_model['source']
-    if not os.path.exists(input_path):
-        os.makedirs(input_path)
-
-    resized_path = base_path + df_model['source'] + '-resized'
-    if not os.path.exists(resized_path):
-        os.makedirs(resized_path)
-
-    output_path = base_path + df_model['source'] + '-enc'
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-
-    residual_path = base_path + df_model['source'] + '-residual'
-    if not os.path.exists(residual_path):
-        os.makedirs(residual_path)
-
-    swap_path = base_path + df_model['source'] + '-enc-swap'
-    if not os.path.exists(swap_path):
-        os.makedirs(swap_path)
-
-    df_model['input_path'] = input_path
-    df_model['resized_path'] = resized_path
-    df_model['residual_path'] = residual_path
-    df_model['output_path'] = output_path
-    df_model['swap_path'] = swap_path
-    df_model['example'] = None
-
-size = 400
-
-def clear_previous():
-    for df_model in df_models:
-        output_path = df_model['output_path'] 
-        swap_path = df_model['swap_path']
-        residual_path = df_model['residual_path']
-        files = glob(f'{output_path}/*') + glob(f'{swap_path}/*') + glob(f'{residual_path}/*')
-        for file in files:
-            os.remove(file)
-
-def compile_examples(example):
-    output = Image.new('RGB', (size*3, size*len(df_models)), 'white')
-    for idx, df_model in enumerate(df_models):
-        img_path = pathlib.Path(glob(df_model['output_path'] + '/' + '*.png')[0])
-        full_stem = img_path.stem + img_path.suffix
-        clean_path = df_model['resized_path'] + '/' + full_stem 
-        encoded_path = df_model['output_path'] + '/' + full_stem
-        swapped_path = df_model['swap_path'] + '/' + full_stem
-        # residual_path = df_model['residual_path'] + '/' + full_stem
-
-        clean = Image.open(clean_path)
-        encoded = Image.open(encoded_path)
-        swapped = Image.open(swapped_path)
-        # residual = Image.open(residual_path)
-
-        output.paste(clean, (0, idx*size))
-        # output.paste(residual, (size, idx*size))
-        output.paste(encoded, (size, idx*size))
-        output.paste(swapped, (size*2, idx*size))
-
-    output.save(f'./results/stegastamp-{example}.png')
-
-def bitstring_to_bytes(s):
-    return int(s, 2).to_bytes((len(s) + 7) // 8, byteorder='big')
-
-def secret_to_string(secret):
-    secret = ''.join(secret)
-    byte_string = bitstring_to_bytes(secret)
-    print(byte_string)
-    print(byte_string.decode('utf-8'))
+global steganogan
+steganogan = None
 
 def get_steganogan(architecture):
 
-    steganogan_kwargs = {
-        'cuda': True,
-        'verbose': False,
-        # 'depth': 4,
-    }
+    global steganogan
+    if(steganogan == None):
 
-    steganogan_kwargs['path'] = f'./steganogan/pretrained/{architecture}.steg'
-    # steganogan_kwargs['architecture'] = 'dense'
+        steganogan_kwargs = {
+            'cuda': True,
+            'verbose': False,
+            # 'depth': 4,
+        }
 
-    return SteganoGAN.load(**steganogan_kwargs)
+        steganogan_kwargs['path'] = f'./steganogan/pretrained/{architecture}.steg'
+        # steganogan_kwargs['architecture'] = 'dense'
+        steganogan = SteganoGAN.load(**steganogan_kwargs)
 
-def channel_code(mode, secret):
-    secret_string = ''.join([str(s) for s in secret])
-    encoded = subprocess.check_output(f'env/bin/python3 channel_coder.py {mode} {secret_string}',shell=True)
-    encoded = encoded.splitlines()[-1].decode('utf8')
-    encoded_array = []
-    for i in encoded:
-        encoded_array.append(int(i))
-    return encoded_array
-    # secret = ast.literal_eval(secret)
+    return steganogan
 
-def encode(preswap, input_dir, output_dir, architecture):
+
+def encode(input_dir, output_dir, amount, secret_file, architecture):
     """Given loads a pretrained pickle, encodes the image with it."""
-    similarities = []
+    secrets = pickle.load(open(secret_file,'rb'))
     steganogan = get_steganogan(architecture)
-    images = glob(f'{input_dir}/*.png')
-    random.shuffle(images)
-    images = images[:25]
-    analysis = pickle.load(open(f'{input_dir}/analysis.bin','rb'))
-    pbar = tqdm(images)
-    for image in pbar:
-        path = pathlib.Path(image)
-        full_stem = path.stem + '.png'
-        try:
-            gt = analysis[image][0]
-        except KeyError as e:
-            continue
-        encoded = channel_code('encode',gt)
-        steganogan.encode(image, f'{output_dir}/{full_stem}', encoded)
-        undecoded = steganogan.decode(f'{output_dir}/{full_stem}')
-        undecoded = [int(i) for i in undecoded]
-        decoded = channel_code('decode',undecoded)
+    images = glob(input_dir + '/*.png')
+    images = images[:amount]
+    for step, image in enumerate(tqdm(images)):
+        output_image = pathlib.Path(image).stem + '.png'
+        gt = secrets[step]
+        steganogan.encode(image, f'{output_dir}/{output_image}', gt)
 
-        try:
-            analyzed = subprocess.check_output(f'env/bin/python3 analyze_face.py {output_dir}/{full_stem}',shell=True)
-            analyzed = analyzed.splitlines()[-1].decode('utf8')
-            analyzed = ast.literal_eval(analyzed)
-        except Exception as e:
-            print(e)
-            continue
-
-        try:
-            score = dot(analyzed, decoded)/(norm(analyzed)*norm(decoded))
-        except:
-            continue
-        # if np.isnan(cos_sim):
-        #     print(gt, dec)
-        #     print(cos_sim)
-        #     breakpoint()
-            # gt_secrets = torch.T
-        preswap.append(score)
-        pbar.set_description(f'score: {np.mean(preswap)}')
-
-    return preswap 
-
-def decode(swapped, input_dir, architecture):
+def decode(input_image, secret_file, architecture):
     """Given loads a pretrained pickle, encodes the image with it."""
-    similarities = []
+    output_secrets = []
     steganogan = get_steganogan(architecture)
-    images = glob(f'{input_dir}/*.png')
-    pbar = tqdm(images)
-    for image in pbar:
-        path = pathlib.Path(image)
-        full_stem = path.stem + '.png'
-        undecoded = steganogan.decode(f'{image}')
-        undecoded = [int(i) for i in undecoded]
-        decoded = channel_code('decode',undecoded)
-
-        try:
-            analyzed = subprocess.check_output(f'env/bin/python3 analyze_face.py {image}',shell=True)
-            analyzed = analyzed.splitlines()[-1].decode('utf8')
-            analyzed = ast.literal_eval(analyzed)
-        except Exception as e:
-            print(e)
-            continue
-
-        try:
-            score = dot(analyzed, decoded)/(norm(analyzed)*norm(decoded))
-        except:
-            continue
-        # if np.isnan(cos_sim):
-        #     print(gt, dec)
-        #     print(cos_sim)
-        #     breakpoint()
-            # gt_secrets = torch.T
-
-        swapped.append(score)
-        pbar.set_description(f'score: {np.mean(swapped)}')
-    
-    return swapped
-
-
+    decoded = steganogan.decode(input_image)
+    decoded = [int(i) for i in decoded]
+    output_secrets.append(decoded)
+    pickle.dump(output_secrets, open(secret_file, 'wb'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Specify GPU and run')
-    parser.add_argument('filename')
+    parser.add_argument('input_dir')
     parser.add_argument('mode')
-    parser.add_argument('--secret')
-    parser.add_argument('--analysis')
+    parser.add_argument('secret_file')
+    parser.add_argument('architecture')
+    parser.add_argument('--amount')
+    parser.add_argument('--output_dir')
 
     args = parser.parse_args()
 
     if(args.mode == 'encode'):
-        encode(args.filename, args.secret)
+        encode(args.input_dir, args.output_dir, args.amount, args.secret_file, args.architecture)
     elif(args.mode == 'decode'):
-        decode(args.filename, args.analysis)
+        decode(args.input_dir, args.secret_file, args.architecture)
--- run_stegastamp.py
+++ run_stegastamp.py
@@ -1,4 +1,3 @@
-import subprocess 
 from glob import glob
 from PIL import Image, ImageOps
 import numpy as np
@@ -6,131 +5,19 @@ import tensorflow as tf
 import tensorflow.contrib.image
 from tensorflow.python.saved_model import tag_constants
 from tensorflow.python.saved_model import signature_constants
-import os
 import pathlib
-import torch
-from torch import nn
-import ast
 from tqdm import tqdm
 import pickle
-import json
 import argparse
 
-cos = torch.nn.CosineSimilarity(dim=1)
-
-class ChannelDecoder(nn.Module):
-    def __init__(self, secret_size, model_size):
-        super().__init__()
-        if(model_size == 'small'):
-            layers = nn.Sequential(
-                nn.Linear(100, secret_size * 6),
-                nn.ReLU(),
-                nn.Linear(secret_size * 6, secret_size * 3),
-                nn.ReLU(),
-                nn.Linear(secret_size * 3, secret_size),
-                nn.ReLU(),
-            )
-        elif(model_size == 'big'):
-            layers = nn.Sequential(
-                nn.Linear(100, secret_size * 6),
-                nn.ReLU(),
-                nn.Linear(secret_size * 6, secret_size * 4),
-                nn.ReLU(),
-                nn.Linear(secret_size * 4, secret_size * 2),
-                nn.ReLU(),
-                nn.Linear(secret_size * 2, secret_size),
-                nn.ReLU(),
-            )
-        self.main = layers
-
-    def forward(self, secret):
-        output = self.main(secret)
-        return output
-
-class ChannelEncoder(nn.Module):
-    def __init__(self, secret_size, model_size):
-        super().__init__()
-        if(model_size == 'small'):
-            layers = nn.Sequential(
-                nn.Linear(secret_size, secret_size * 3),
-                nn.ReLU(),
-                nn.Linear(secret_size * 3, secret_size * 6),
-                nn.ReLU(),
-                nn.Linear(secret_size * 6, 100),
-                nn.ReLU(),
-            )
-        elif(model_size == 'big'):
-            layers = nn.Sequential(
-                nn.Linear(secret_size, secret_size * 2),
-                nn.ReLU(),
-                nn.Linear(secret_size * 2, secret_size * 4),
-                nn.ReLU(),
-                nn.Linear(secret_size * 4, secret_size * 6),
-                nn.ReLU(),
-                nn.Linear(secret_size * 6, 100),
-                nn.ReLU(),
-            )
-        self.main = layers
-
-    def forward(self, secret):
-        output = self.main(secret)
-        return output
-
-
-
-base_path = './test_data/vidtimit/process'
-
-df_models = json.load(open('df_models.json','r')) 
-
-for df_model in df_models:
-    input_path = base_path + df_model['source']
-    if not os.path.exists(input_path):
-        os.makedirs(input_path)
-
-    resized_path = base_path + df_model['source'] + '-resized'
-    if not os.path.exists(resized_path):
-        os.makedirs(resized_path)
-
-    output_path = base_path + df_model['source'] + '-enc'
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-
-    residual_path = base_path + df_model['source'] + '-residual'
-    if not os.path.exists(residual_path):
-        os.makedirs(residual_path)
-
-    swap_path = base_path + df_model['source'] + '-enc-swap'
-    if not os.path.exists(swap_path):
-        os.makedirs(swap_path)
-
-    df_model['input_path'] = input_path
-    df_model['resized_path'] = resized_path
-    df_model['residual_path'] = residual_path
-    df_model['output_path'] = output_path
-    df_model['swap_path'] = swap_path
-    df_model['example'] = None
-
-size = 400
-
-channel_encoder = ChannelEncoder(14, 'small')
-channel_decoder = ChannelDecoder(14, 'small')
-channel_encoder.load_state_dict(torch.load('checkpoints/channel_encoder_s22'))
-channel_decoder.load_state_dict(torch.load('checkpoints/channel_decoder_s22'))
-channel_encoder.eval()
-channel_decoder.eval()
-channel_encoder = channel_encoder.cuda()
-channel_decoder = channel_decoder.cuda()
-
-dataset_size = 1000
-
-def stegastamp_encode(input_dir, output_dir):
+def encode(input_dir, output_dir, amount, secret_file):
 
     model = 'StegaStamp/saved_models/stegastamp_pretrained'
 
+    secrets = pickle.load(open(secret_file,'rb'))
 
     files_list = glob(input_dir + '/*.png')
-    size = int(len(files_list) * 0.02)
-    files_list = files_list[:25]
+    files_list = files_list[:amount]
 
     sess = tf.InteractiveSession(graph=tf.Graph())
 
@@ -146,66 +33,39 @@ def stegastamp_encode(input_dir, output_dir):
     output_stegastamp = tf.get_default_graph().get_tensor_by_name(output_stegastamp_name)
     output_residual = tf.get_default_graph().get_tensor_by_name(output_residual_name)
 
-    width = 400
-    height = 400
+    size = (400, 400)
 
-    secrets = []
+    for step, filename in enumerate(tqdm(files_list)):
+        path = pathlib.Path(filename)
+        full_stem = path.stem + path.suffix 
+        image = Image.open(filename).convert("RGB")
+        image = np.array(ImageOps.fit(image,size),dtype=np.float32)
+        image /= 255.
 
-    if output_dir is not None:
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-        size = (width, height)
+        secret = secrets[step] 
+        feed_dict = {input_secret:[secret],
+                    input_image:[image]}
 
-        analysis = pickle.load(open(f'{input_dir}/analysis.bin','rb'))
-        for filename in tqdm(files_list):
-            path = pathlib.Path(filename)
-            full_stem = path.stem + path.suffix 
-            image = Image.open(filename).convert("RGB")
-            im = ImageOps.fit(image,size)
-            image = np.array(ImageOps.fit(image,size),dtype=np.float32)
-            image /= 255.
+        hidden_img, residual = sess.run([output_stegastamp, output_residual],feed_dict=feed_dict)
 
-            try:
-                secret = analysis[filename][0]
-            except Exception as e:
-                print(e)
-                continue
+        rescaled = (hidden_img[0] * 255).astype(np.uint8)
+        raw_img = (image * 255).astype(np.uint8)
 
-            secret = torch.Tensor(secret).cuda()
-            secret = channel_encoder(secret)
+        residual = residual[0]+.5
 
-            secret = torch.round(torch.clip(secret, 0, 1)).tolist()
-            secrets.append(secret)
+        # im = Image.fromarray(np.squeeze(np.array(residual.astype(np.uint8))))
+        # im.save(args.save_dir + 'raw_residual/'+save_name+'_residual.png')
 
-            feed_dict = {input_secret:[secret],
-                        input_image:[image]}
+        residual = (residual * 255).astype(np.uint8)
+        
+        im = Image.fromarray(np.array(rescaled))
+        new_filename = output_dir + '/' + full_stem
+        im.save(new_filename)
 
-            hidden_img, residual = sess.run([output_stegastamp, output_residual],feed_dict=feed_dict)
-
-            rescaled = (hidden_img[0] * 255).astype(np.uint8)
-            raw_img = (image * 255).astype(np.uint8)
-
-            residual = residual[0]+.5
-
-            # im = Image.fromarray(np.squeeze(np.array(residual.astype(np.uint8))))
-            # im.save(args.save_dir + 'raw_residual/'+save_name+'_residual.png')
-
-            residual = (residual * 255).astype(np.uint8)
-            
-            im = Image.fromarray(np.array(rescaled))
-            new_filename = output_dir + '/' + full_stem
-            im.save(new_filename)
-
-            # im = Image.fromarray(np.squeeze(np.array(residual)))
-            # im.save(args.save_dir + '/' + full_stem)
-    return secrets
-
-def stegastamp_decode(input_dir):
+def decode(input_dir, secret_file):
     model = 'StegaStamp/saved_models/stegastamp_pretrained'
 
     files_list = glob(input_dir + '/*.png')
-
-    unreadable = 0
 
     sess = tf.InteractiveSession(graph=tf.Graph())
 
@@ -217,18 +77,9 @@ def stegastamp_decode(input_dir):
     output_secret_name = model.signature_def[signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY].outputs['decoded'].name
     output_secret = tf.get_default_graph().get_tensor_by_name(output_secret_name)
 
-    similarities = []
+    secrets = []
 
     for filename in tqdm(files_list):
-        try:
-            analyzed = subprocess.check_output(f'env/bin/python3 analyze_face.py {filename}',shell=True)
-            analyzed = analyzed.splitlines()[-1].decode('utf8')
-            analyzed = ast.literal_eval(analyzed)
-        except Exception as e:
-            print(e)
-            unreadable += 1
-            continue
-
         image = Image.open(filename).convert("RGB")
         image = np.array(ImageOps.fit(image,(400, 400)),dtype=np.float32)
         image /= 255.
@@ -237,70 +88,26 @@ def stegastamp_decode(input_dir):
 
         secret = sess.run([output_secret],feed_dict=feed_dict)[0][0]
 
-        secret = torch.Tensor(secret).cuda()
-        secret = channel_decoder(secret)
-        secret = torch.clip(secret, 0, 1)
-        secret = torch.round(secret).cpu()
+        secret = np.array(secret)
+        secret = np.clip(secret, 0, 1)
+        secret = np.round(secret)
+        secret = secret.tolist()
 
-        analyzed = torch.Tensor(analyzed)
-
-        similarity = cos(secret[None], analyzed[None])
-        similarities.append(similarity.item())
+        secrets.append(secret)
     
-    return similarities, unreadable
-
-
-# def run_stegastamp_encode(source):
-#     # python3 encode_image.py ../test_data/vidtimit/processfadg0 ../test_data/vidtimit/processfadg-enc
-#     stega_cmd = f'/home/luna/anaconda3/envs/stegastamp/bin/python3 encode_image.py ../test_data/vidtimit/process{source} ../test_data/vidtimit/process{source}-enc'
-
-#     subprocess.run(stega_cmd, shell=True, cwd='./StegaStamp')
-
-# def run_stegastamp_decode(source):
-#     # python3 encode_image.py ../test_data/vidtimit/processfadg0 ../test_data/vidtimit/processfadg-enc
-#     stega_cmd = f'/home/luna/anaconda3/envs/stegastamp/bin/python3 encode_image.py ../test_data/vidtimit/process{source} ../test_data/vidtimit/process{source}-enc'
-
-#     subprocess.run(stega_cmd, shell=True, cwd='./StegaStamp')
-def clear_previous():
-    for df_model in df_models:
-        output_path = df_model['output_path'] 
-        swap_path = df_model['swap_path']
-        residual_path = df_model['residual_path']
-        files = glob(f'{output_path}/*') + glob(f'{swap_path}/*') + glob(f'{residual_path}/*')
-        for file in files:
-            os.remove(file)
-
-def compile_examples(example):
-    output = Image.new('RGB', (size*3, size*len(df_models)), 'white')
-    for idx, df_model in enumerate(df_models):
-        img_path = pathlib.Path(glob(df_model['output_path'] + '/' + '*.png')[0])
-        full_stem = img_path.stem + img_path.suffix
-        clean_path = df_model['resized_path'] + '/' + full_stem 
-        encoded_path = df_model['output_path'] + '/' + full_stem
-        swapped_path = df_model['swap_path'] + '/' + full_stem
-        # residual_path = df_model['residual_path'] + '/' + full_stem
-
-        clean = Image.open(clean_path)
-        encoded = Image.open(encoded_path)
-        swapped = Image.open(swapped_path)
-        # residual = Image.open(residual_path)
-
-        output.paste(clean, (0, idx*size))
-        # output.paste(residual, (size, idx*size))
-        output.paste(encoded, (size, idx*size))
-        output.paste(swapped, (size*2, idx*size))
-
-    output.save(f'./results/stegastamp-{example}.png')
+    pickle.dump(secrets, open(secret_file,'wb'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Specify GPU and run')
-    parser.add_argument('filename')
+    parser.add_argument('input_dir')
     parser.add_argument('mode')
-    parser.add_argument('secret')
+    parser.add_argument('secret_file')
+    parser.add_argument('--amount')
+    parser.add_argument('--output_dir')
 
     args = parser.parse_args()
 
     if(args.mode == 'encode'):
-        stegastamp_encode()
+        encode(args.input_dir, args.output_dir, args.amount, args.secret_file)
     elif(args.mode == 'decode'):
-        stegastamp_decode()
+        decode(args.input_dir, args.secret_file)
